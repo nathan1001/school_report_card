5ce7b89b61e26a05f74a8e98d132b0500083df72
Nathan Yaw Oppong Donkor <gerrard8donkor@gmail.com>
2018-11-28 15:27:06 +0000

    Test firebase app working now with css

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+const firebase = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+var config = {
+    apiKey: "AIzaSyDdEzILP3T5nawNBYkFMlPi_njMNF6t2cY",
+    authDomain: "school-eport-card.firebaseapp.com",
+    databaseURL: "https://school-eport-card.firebaseio.com",
+    projectId: "school-eport-card",
+    storageBucket: "school-eport-card.appspot.com",
+    messagingSenderId: "461648540722"
+  };
+  
+const firebaseApp = firebase.initializeApp(config);
+
+function getFacts(){
+    const ref = firebaseApp.database().ref('facts');
+    return ref.once('value').then(snap => snap.val());
+}
+
+const app = express();
+
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+app.get('/', (request,response) =>{
+    // response.set('Cache-Control','public,max-age=300,s-maxage=600');
+    getFacts().then(facts =>{
+        response.render('index',{ facts });
+    });
+});
+
+
+exports.app = functions.https.onRequest(app);
+
