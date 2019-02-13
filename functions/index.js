fca97c9a86fb20b0f4b0ec267ca5221dbf758597
Nathan Yaw Oppong Donkor <gerrard8donkor@gmail.com>
2019-02-13 09:48:10 +0000

    Pushing to make everything current

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,14 @@ const functions = require('firebase-functions');
 const express = require('express');
 const engines = require('consolidate');
 const firebase = require('firebase-admin');
+//var firebase = require('firebase');
+//var firebaseui = require('firebaseui');
+
+//const auth = firebase.auth();
+
+//auth.signInWithEmailAndPassword(email,pass);
+//auth.createUserWithEmailAndPassword(email,pass);
+//auth.onAuthStateChanged(firebaseUser =>{});
 
 
 var config = {
@@ -19,19 +27,38 @@ function getFacts(){
     const ref = firebaseApp.database().ref('facts');
     return ref.once('value').then(snap => snap.val());
 }
+const login =(email,password) => {
+
+}
 
 const app = express();
+
+// app.engine('html', engines.swig); // take note, using 'html', not 'ejs' or 'pug'..
+// app.set('view engine', 'html');
 
 app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
 
-app.get('/', (request,response) =>{
+app.get('/login', (request,response) =>{
     // response.set('Cache-Control','public,max-age=300,s-maxage=600');
     getFacts().then(facts =>{
-        response.render('index',{ facts });
+        response.render('facts',{ facts });
     });
 });
+app.get('/',function(req,res){
+       
+  res.sendFile('index');
+
+});
+
+// app.get('/login', (request,response) =>{
+//   // response.set('Cache-Control','public,max-age=300,s-maxage=600');
+//   getFacts().then(facts =>{
+//       response.render('login',{ facts });
+//   });
+// });
+
 
 
 exports.app = functions.https.onRequest(app);
